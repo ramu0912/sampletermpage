04741b34d7a86af91d7940216bf311e7cedf05d7
Ramu Sura <ramu.sura09@gmail.com>
2021-12-24 12:22:52 +0530

    updated

---
+++ js/form.js
@@ -0,0 +1,33 @@
+$('.field.example form')
+  .form({
+    on: 'blur',
+    fields: {
+      empty: {
+        identifier  : 'empty',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a value'
+          }
+        ]
+      },
+      dropdown: {
+        identifier  : 'dropdown',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please select a dropdown value'
+          }
+        ]
+      },
+      checkbox: {
+        identifier  : 'checkbox',
+        rules: [
+          {
+            type   : 'checked',
+            prompt : 'Please check the checkbox'
+          }
+        ]
+      }
+    }
+  })
